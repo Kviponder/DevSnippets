7578789cf631ea2752784d2f9a476e8affed618e
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-30 20:13:56 -0400

    Deleted routes file

--- dev-snippets/Server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const snippetRoutes = require('./snippetRoutes');
-
-router.use('/users', userRoutes);
-router.use('/snippet', snippetRoutes);
-
-module.exports = router;
