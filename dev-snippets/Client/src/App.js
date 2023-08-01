adbbd4e3faf65cf3ae85349568803b16b7a5b320
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-31 20:40:02 -0400

    Created Header.js

--- dev-snippets/Client/src/App.js
+++ dev-snippets/Client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import SnippetList from './components/SnippetList';
 import SignupForm from './components/SignupForm';
+import Header from './components/header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/snippets" element={<SnippetList />} />
